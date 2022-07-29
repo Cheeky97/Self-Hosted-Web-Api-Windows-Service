8f27f727e33c368c4ea73e15d0d60927dfb1b85b
Cheeky97 <98615289+Cheeky97@users.noreply.github.com>
2022-07-29 12:08:24 +0100

    User Management Service Project

---
+++ UserManagement_Service/HostingService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Owin.Hosting;
+using SelfHostedAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagement_Service
+{
+    [RunInstaller(true)]
+    public partial class HostingService : ServiceBase
+    {
+
+        public HostingService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            SelfHostedAgent selfHostedAPI = new SelfHostedAgent();
+            selfHostedAPI.StartAgent();
+        }
+
+        protected override void OnStop()
+        {
+            
+        }
+    }
+}
